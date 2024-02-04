da1befd4c93078e0daf62badbced2519bf0c54c3
alizeh-zaidi <zaidi.al@northeastern.edu>
2024-02-04 13:24:10 -0500

    idk

---
+++ my-app/src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const sum: number = 4
+
+
+  return (
+    <div className="App">
+      <p> The sum of 2 + 2 is {sum}!</p>
+    </div>
+  );
+}
+
+export default App;
+
+
